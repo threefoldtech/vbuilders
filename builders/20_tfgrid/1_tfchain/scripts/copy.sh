b4859bc4e1760570acdd9bc06254ddd6235ee34c
Rob Van Mieghem <robvanmieghem@gmail.com>
2023-02-05 21:20:59 +0100

    copy the standard chainspecs in the tfgrid/tfchain builder

--- builders/20_tfgrid/1_tfchain/scripts/copy.sh
+++ builders/20_tfgrid/1_tfchain/scripts/copy.sh
@@ -12,6 +12,11 @@ fi
 mkdir -p /myhost/ubuntu/tfchain/bin
 cp /code/threefoldtech/tfchain/substrate-node/target/release/tfchain /myhost/ubuntu/tfchain/bin/
 
+mkdir -p /myhost/ubuntu/tfchain/etc
+rm -rf /myhost/ubuntu/tfchain/etc/*
+
+cp -R cp /code/threefoldtech/tfchain/substrate-node/chainspecs /myhost/ubuntu/tfchain/etc/
+
 echo " ** COPY DONE"
 
 
