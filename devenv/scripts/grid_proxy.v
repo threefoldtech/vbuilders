358685bab10ca364d2611bd2ebf5f6c1933be6e8
Thunder <mahmmoud.hassanein@gmail.com>
2023-02-05 13:36:19 +0200

    Update grid_proxy script.

--- devenv/scripts/grid_proxy.v
+++ devenv/scripts/grid_proxy.v
@@ -1,9 +1,16 @@
 import threefoldtech.vgrid.gridproxy.model as gmodel
 import threefoldtech.vgrid.gridproxy as gp
 
-
-pub fn (mut grd Grid3)find_nodes()? []gp.Node{
+pub fn find_nodes()? []gmodel.Node{
 	mut gproxy := gp.get(.test, false)
 	nodes := gproxy.get_nodes()?
 	return nodes
+}
+
+
+fn main(){
+	println("Searching for available nodes")
+	nodes := find_nodes()?
+	println("nodes found!")
+	println(nodes)
 }
