ef4dc499960bac6d774d80bfad062207f017af2f
despiegk <>
2023-02-09 11:27:32 +0300

    builders

--- scripts/installers/docker.sh
+++ scripts/installers/docker.sh
@@ -34,8 +34,6 @@ function os_update {
 
 apt-get remove docker docker-engine docker.io containerd runc -y 
 
-set -ex
-
 mkdir -p /etc/apt/keyrings
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -51,3 +49,4 @@ apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 docker run hello-world
 
+echo "*** INSTALL DOCKER OK ***"
