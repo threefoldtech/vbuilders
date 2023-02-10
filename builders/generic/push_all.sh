96b95c3895fa55365ca144fc7174c5a0b8b96cb3
despiegk <>
2023-02-10 09:49:26 +0300

    builders

--- builders/generic/push_all.sh
+++ builders/generic/push_all.sh
@@ -4,7 +4,7 @@ docker login
 # for name in "base0" "rust0" "rustbuilder" "base" "gobuilder" "nodejsbuilder" "vbuilder" "nsc" "natstools" "3bot"
 for name in "base0"
 do
-    docker push $name
+    docker push despiegk/$name:latest
 done
 
 echo "*** BUILD OF ALL DOCKERS WORKED***"
