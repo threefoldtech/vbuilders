5337193ec6fec0800d6a50ee0b7eb81f111cab95
Rob Van Mieghem <robvanmieghem@gmail.com>
2023-02-10 09:52:14 +0100

    build weblets

--- builders/tfgrid/build_all.sh
+++ builders/tfgrid/build_all.sh
@@ -2,7 +2,7 @@ set -ex
 
 source versions.sh
 
-for name in "1_tfchain" "2_tfchain_graphql" "3_tfchain_activation_service" "4_gridproxy" "5_dashboard" "7_zflist"
+for name in "1_tfchain" "2_tfchain_graphql" "3_tfchain_activation_service" "4_gridproxy" "5_dashboard" "6_weblets" "7_zflist"
 do
     echo "START BUILDING $name"
     pushd $name
