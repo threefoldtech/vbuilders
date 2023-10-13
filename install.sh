aa17e80a51ddbb3c6ea8a90c79be411ca7a55182
despiegk <>
2023-10-13 10:00:12 +0300

    ...

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ SOURCE=${BASH_SOURCE[0]}
 DIR_OF_THIS_SCRIPT="$( dirname "$SOURCE" )"
 ABS_DIR_OF_SCRIPT="$( realpath $DIR_OF_THIS_SCRIPT )"
 mkdir -p ~/.vmodules/threefoldtech
+rm -f ~/.vmodules/threefoldtech/builders
 ln -s $ABS_DIR_OF_SCRIPT/builders ~/.vmodules/threefoldtech/builders
